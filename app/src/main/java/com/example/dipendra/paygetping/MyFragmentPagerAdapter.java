b092b39da77c7080a307a2d18a8a8520ef579842
gadabout-geek <dipendrabana@gmail.com>
2017-03-08 22:23:42 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/dipendra/paygetping/MyFragmentPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.example.dipendra.paygetping;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by dipendra on 20/02/17.
+ */
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> arrayList;
+    public MyFragmentPagerAdapter(FragmentManager fm, ArrayList<Fragment> arrayList) {
+        super(fm);
+        this.arrayList = arrayList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return arrayList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return arrayList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if(position == 0){
+            return "Pay";
+        }
+        if(position == 1){
+            return "Get";
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/com/example/dipendra/paygetping/PayFragment.java
@@ -0,0 +1,22 @@
+package com.example.dipendra.paygetping;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class PayFragment extends Fragment {
+
+
+    public PayFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_pay, container, false);
+    }
+
+}
